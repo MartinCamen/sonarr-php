9a5877c52830261e969533e9e5569e06c3c2202d
Martin Camenius <camen@camensky.se>
2026-01-05 00:18:17 +0100

    Rename client

---
+++ src/Client/SonarrRestClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MartinCamen\Sonarr\Client;
+
+use MartinCamen\ArrCore\Client\RestClient as CoreRestClient;
+
+/** @link https://sonarr.tv/docs/api/ */
+class SonarrRestClient extends CoreRestClient {}
